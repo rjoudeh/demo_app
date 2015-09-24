330e55d8d7474c50dd7642b4e821abacc95d59e7
Rania Joudeh <rania.joudeh@gmail.com>
2015-09-24 16:38:50 +0300

    create microposts resource + create user/micropost relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919211928) do
+ActiveRecord::Schema.define(version: 20150924120033) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
